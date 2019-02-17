317ce175053cba2413cda2765123a42a0b9d1551
TechitCS <techit.c@ku.th>
2019-02-17 15:10:36 +0700

    formage0-99

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
 import { User } from '../user';
 
 @Component({
@@ -16,17 +16,42 @@ export class FormComponent implements OnInit {
 
   ngOnInit() {
     this.formGroup = this.formBuild.group({
-      firstName:[''],
-      lastName:[''],
-      Email:[''],
-      age:['']
+      firstName:['',[Validators.required,Validators.minLength(2)]],
+      lastName:['',[Validators.required,Validators.minLength(2)]],
+      Email:['',[Validators.email]],
+      age:['',[Validators.min(0),Validators.max(99)]]
     })
   }
+  EmailValidator(control:AbstractControl){
+    const value = control.value;
+    if(value && value.includes('@')){
+      return null;
+    }
+    return{
+      Email:true
+    }
+
+  }
   onSubmit(form: FormGroup){
-    const{firstName,lastName,Email,age} = form.value;
-    console.log(firstName,lastName,Email);
-    const user = new User(firstName,lastName,Email,age);
-    console.log(user);
+    console.log(form.invalid);
+    console.log((<FormControl>form.get('firstName')).errors)
+    console.log((<FormControl>form.get('lastName')).errors)
+    console.log((<FormControl>form.get('Email')).errors)
+    console.log((<FormControl>form.get('age')).errors)
+    if(form.invalid){
+      /*const{firstName,lastName,Email,age} = form.value;
+      console.log(firstName,lastName,Email);
+      const user = new User(firstName,lastName,Email,age);
+      console.log(user);
+      */
+    }else{
+      ['firstName','lastName','age','email'].forEach((key:string)=>{
+        console.log(form.get(key).touched);
+        form.get(key).markAsTouched();
+        console.log(form.get(key).touched);
+      })
+    }
+    
   }
 
 }
